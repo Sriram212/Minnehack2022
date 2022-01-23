feee26bded6f66cb16efca85309f9399a0ebac14
baileyssachs <47926426+baileyssachs@users.noreply.github.com>
2022-01-23 00:38:29 -0600

    steamlit page

--- webpage.py
+++ webpage.py
@@ -3,3 +3,26 @@ import pandas as pd
 import numpy as np
 
 st.title('Privacy Helper')
+
+st.sidebar.header("Select a company to view their data policy.")
+def user_input_company():
+    selection = st.sidebar.selectbox('Company',('Amazon','Facebook', 'Google', 'Grubhub', 'Instagram', 'LinkedIn', 'Snapchat', 'Tiktok', 'Twitch', 'Uber', 'Yahoo'))
+    return selection
+
+company = user_input_company()
+
+st.subheader("Here is a sample of the Privacy Policy we are looking at.")
+
+company_file_name = company + 'DataPolicy.txt'
+company_file_path = 'privacy_policy_files/' + company_file_name
+
+text = ""
+
+with open(company_file_path, encoding="utf-8") as f:
+    for i in range(15):
+        text += next(f).strip()
+
+st.markdown(text)
+
+st.subheader("Here is the highlights of the Privacy Policy.")
+st.markdown("sample highlights")
