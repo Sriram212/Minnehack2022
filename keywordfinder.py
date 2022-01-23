d635a05855ea1b1ce9b8f66d52ae0ea26b4b567a
Sriram212 <sriramnutulapati@gmail.com>
2022-01-23 00:43:52 -0600

    streamlit update

--- keywordfinder.py
+++ keywordfinder.py
@@ -1,16 +1,53 @@
 import yake
-kw_extractor = yake.KeywordExtractor()
+from keybert import KeyBERT
+import re
+
+#kw_extractor = yake.KeywordExtractor()
 
 text = ""
+stopwords = ""
+keywords = ""
 
-with open('test.txt', encoding="utf-8") as f:
+with open('policies.txt', encoding="utf-8") as f:
     text += f.read()
-    
-language = "en"
-max_ngram_size = 3
-deduplication_threshold = 0.9
-numOfKeywords = 50
-custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, top=numOfKeywords, features=None)
-keywords = custom_kw_extractor.extract_keywords(text)
-for kw in keywords:
-    print(kw)
+with open('stopwords.txt', encoding="utf-8") as f:
+    stopwords += f.read()
+with open('keywords.txt', encoding="utf-8") as f:
+    keywords += f.read()
+stopwords_list = stopwords.split('\n')
+keywords_list = keywords.split('\n')
+text = text.lower()
+
+# language = "en"
+# max_ngram_size = 1
+# deduplication_threshold = 0.9
+# numOfKeywords = 50
+# custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, top=numOfKeywords, features=None, stopwords=stopwords)
+# keywords = custom_kw_extractor.extract_keywords(text)
+# for kw in keywords:
+#     print(kw)
+
+kw_model = KeyBERT()
+
+output = kw_model.extract_keywords(text, candidates=keywords_list, keyphrase_ngram_range=(1, 1), top_n=25, diversity=0.5, stop_words=stopwords_list)
+
+linkedin_text = ""
+
+with open('privacy_policy_files/LinkedinDataPolicy.txt', encoding="utf-8") as f:
+    linkedin_text += f.read()
+
+sentences_dict = {}
+
+line_end_chars = "!", "?", '\n'
+regexPattern = '|'.join(map(re.escape, line_end_chars))
+line_list = re.split(regexPattern, linkedin_text)
+for i in output:
+    word = i[0]
+    count = 0
+    for line in line_list:
+        if word in line.lower() and count < 3:
+            if line not in sentences_dict:
+                print(line + '\n')
+                sentences_dict[line] = 1
+                count += 1
+
--- webpage.py
+++ webpage.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from keybert import KeyBERT
 
 st.title('Privacy Helper')
 
@@ -26,3 +27,22 @@ st.markdown(text)
 
 st.subheader("Here is the highlights of the Privacy Policy.")
 st.markdown("sample highlights")
+
+
+doc = """
+         Supervised learning is the machine learning task of learning a function that
+         maps an input to an output based on example input-output pairs. It infers a
+         function from labeled training data consisting of a set of training examples.
+         In supervised learning, each example is a pair consisting of an input object
+         (typically a vector) and a desired output value (also called the supervisory signal). 
+         A supervised learning algorithm analyzes the training data and produces an inferred function, 
+         which can be used for mapping new examples. An optimal scenario will allow for the 
+         algorithm to correctly determine the class labels for unseen instances. This requires 
+         the learning algorithm to generalize from the training data to unseen situations in a 
+         'reasonable' way (see inductive bias).
+      """
+kw_model = KeyBERT()
+
+keywords = kw_model.extract_keywords(doc, highlight=True)
+st.text(keywords)
+st.markdown(keywords)
