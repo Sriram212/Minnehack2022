c2ca0e7c71d8cd267eb88ac519783dcc05dbf83a
Sriram212 <sriramnutulapati@gmail.com>
2022-01-23 12:09:55 -0600

    function fun

--- keywordfinder.py
+++ keywordfinder.py
@@ -2,91 +2,54 @@ import yake
 from keyBERT import KeyBERT
 import re
 from keyBERT._highlight import highlight_document
-# from keybert._highlight import highlight_document
+from nltk.corpus import stopwords as list_of_eng_stopwords
 
-def write_yellow(f, str_):
-    f.write('<span style="background-color: #FFFF00">%s</span>' % str_)
+class policy_keywords:
+    def getKeyWords(self, keywords_doc=None, stopwords_list=None, policies_text=None, n_words=None):
+        text = ""
+        stopwords = ""
+        keywords = ""
 
-# def substring_in_string(list, word):
-#     for x in list:
-#         if x.startswith(word) and len(word) > 3:
-#             return True
-#     return False
+        with open(policies_text, encoding="utf-8") as f:
+            text += f.read()
+        f.close()
+        with open(stopwords_list, encoding="utf-8") as f:
+            stopwords += f.read()
+        f.close()
+        keywords_list = None
+        if keywords_doc is not None:
+            with open(keywords_doc, encoding="utf-8") as f:
+                keywords += f.read()
+            f.close()
+            keywords_list = keywords.split('\n')
+        stopwords_list = stopwords.split('\n')
+        text = text.lower()
 
-#kw_extractor = yake.KeywordExtractor()
+        output = KeyBERT.extract_keywords(KeyBERT(), docs=text, candidates=keywords_list, keyphrase_ngram_range=(1, 1), top_n=n_words, diversity=0.1, stop_words=stopwords_list)
+        return output
 
-text = ""
-stopwords = ""
-keywords = ""
+    def highlight_doc(self, html_doc_to_write, keywords, policy):
+        policy_text = ""
 
-with open('policies.txt', encoding="utf-8") as f:
-    text += f.read()
-f.close()
-with open('stopwords.txt', encoding="utf-8") as f:
-    stopwords += f.read()
-f.close()
-with open('keywords.txt', encoding="utf-8") as f:
-    keywords += f.read()
-f.close()
-stopwords_list = stopwords.split('\n')
-keywords_list = keywords.split('\n')
-text = text.lower()
+        with open(policy, encoding="utf-8") as f:
+            policy_text += f.read()
+        f.close()
 
-# language = "en"
-# max_ngram_size = 1
-# deduplication_threshold = 0.9
-# numOfKeywords = 50
-# custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, top=numOfKeywords, features=None, stopwords=stopwords)
-# keywords = custom_kw_extractor.extract_keywords(text)
-# for kw in keywords:
-#     print(kw)
+        highlighted_document = highlight_document(policy_text, keywords)
 
-output = KeyBERT.extract_keywords(KeyBERT(), docs=text, candidates=keywords_list, keyphrase_ngram_range=(1, 1), top_n=71, diversity=0.1, stop_words=stopwords_list)
+        line_end_chars = "! ", "?", '. ', '<br>', ';<br>'
+        regexPattern = '|'.join(map(re.escape, line_end_chars))
+        line_list = re.split(regexPattern, highlighted_document)
 
-list_keywords = []
-for temp in output:
-    list_keywords.append(temp[0])
-list_keywords.sort()
-print(list_keywords)
+        f = open(html_doc_to_write, 'w')
+        f.write('<html>')
+        f.write('<p>')
 
-linkedin_text = ""
+        for line in line_list:
+            if '<span style' in line:
+                f.write(line + '.<br><br>')
 
-with open('privacy_policy_files/AmazonDataPolicy.txt', encoding="utf-8") as f:
-    linkedin_text += f.read()
-f.close()
-
-sentences_dict = {}
-
-highlighted_document = highlight_document(linkedin_text, output)
-
-line_end_chars = "! ", "?", '. ', '<br>', ';<br>'
-regexPattern = '|'.join(map(re.escape, line_end_chars))
-line_list = re.split(regexPattern, highlighted_document)
-
-f = open('out.html', 'w')
-f.write('<html>')
-f.write('<p>')
-
-for line in line_list:
-    if '<span style' in line:
-        f.write(line + '.<br><br>')
-
-# for i in output:
-#     word = i[0]
-#     count = 0
-#     for line in line_list:
-#         if word in line.lower() and count < 3:
-#             if line not in sentences_dict:
-#                 for x in line.split():
-#                     if x in list_keywords:
-#                         write_yellow(f, x)
-#                         f.write(' ')
-#                     else:
-#                         f.write(x + ' ')
-#                 sentences_dict[line] = 1
-#                 count += 1
-#                 f.write('.<br><br>')
-f.write('</p>')
-f.write('</html>')
-f.close()
+        f.write('</p>')
+        f.write('</html>')
+        f.close()
 
--- webpage.py
+++ webpage.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from keybert import KeyBERT
+from keywordfinder import policy_keywords
 
 # st.title('Privacy Helper')
 
@@ -29,27 +30,11 @@ from keybert import KeyBERT
 # st.markdown("sample highlights")
 
 
-doc = """
-         Supervised learning is the machine learning task of learning a function that
-         maps an input to an output based on example input-output pairs. It infers a
-         function from labeled training data consisting of a set of training examples.
-         In supervised learning, each example is a pair consisting of an input object
-         (typically a vector) and a desired output value (also called the supervisory signal). 
-         A supervised learning algorithm analyzes the training data and produces an inferred function, 
-         which can be used for mapping new examples. An optimal scenario will allow for the 
-         algorithm to correctly determine the class labels for unseen instances. This requires 
-         the learning algorithm to generalize from the training data to unseen situations in a 
-         'reasonable' way (see inductive bias).
-      """
-kw_model = KeyBERT()
+output = policy_keywords().getKeyWords('keywords.txt', 'stopwords.txt', 'policies.txt', 71)
+output += policy_keywords().getKeyWords(keywords_doc=None, stopwords_list='stopwords.txt', policies_text='policies.txt', n_words=10)
+policy_keywords().highlight_doc('out.html', output, 'privacy_policy_files/AmazonDataPolicy.txt')
 
-f = open('out.html', 'w')
-f.write('<html><p>')
-f.write(kw_model.extract_keywords(doc, highlight=True))
-
-f.write('</p></html>')
-f.close
-
+print(output)
 # keywords = kw_model.extract_keywords(doc, highlight=True)
 # st.text(keywords)
 # st.markdown(keywords)
