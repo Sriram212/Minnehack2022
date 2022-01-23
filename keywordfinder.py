692d748999e9d15ff4dc431b7250f9fc6c00ec62
Sriram212 <sriramnutulapati@gmail.com>
2022-01-23 10:17:43 -0600

    highlightedDoc

--- keywordfinder.py
+++ keywordfinder.py
@@ -1,10 +1,16 @@
 import yake
 from keybert import KeyBERT
 import re
+from keybert._highlight import highlight_document
 
 def write_yellow(f, str_):
     f.write('<span style="background-color: #FFFF00">%s</span>' % str_)
 
+# def substring_in_string(list, word):
+#     for x in list:
+#         if x.startswith(word) and len(word) > 3:
+#             return True
+#     return False
 
 #kw_extractor = yake.KeywordExtractor()
 
@@ -14,10 +20,13 @@ keywords = ""
 
 with open('policies.txt', encoding="utf-8") as f:
     text += f.read()
+f.close()
 with open('stopwords.txt', encoding="utf-8") as f:
     stopwords += f.read()
+f.close()
 with open('keywords.txt', encoding="utf-8") as f:
     keywords += f.read()
+f.close()
 stopwords_list = stopwords.split('\n')
 keywords_list = keywords.split('\n')
 text = text.lower()
@@ -33,43 +42,51 @@ text = text.lower()
 
 kw_model = KeyBERT()
 
-output = kw_model.extract_keywords(text, candidates=keywords_list, keyphrase_ngram_range=(1, 1), top_n=50, diversity=0.1, stop_words=stopwords_list)
+output = kw_model.extract_keywords(text, candidates=keywords_list, keyphrase_ngram_range=(1, 1), top_n=71, diversity=0.1, stop_words=stopwords_list)
 
 list_keywords = []
 for temp in output:
     list_keywords.append(temp[0])
+list_keywords.sort()
 print(list_keywords)
 
 linkedin_text = ""
 
-with open('privacy_policy_files/LinkedinDataPolicy.txt', encoding="utf-8") as f:
+with open('privacy_policy_files/AmazonDataPolicy.txt', encoding="utf-8") as f:
     linkedin_text += f.read()
+f.close()
 
 sentences_dict = {}
 
-line_end_chars = "! ", "? ", '. '
+highlighted_document = highlight_document(linkedin_text, output)
+
+line_end_chars = "! ", "?", '. ', '\n', ';', ' \n'
 regexPattern = '|'.join(map(re.escape, line_end_chars))
-line_list = re.split(regexPattern, linkedin_text)
+line_list = re.split(regexPattern, highlighted_document)
 
 f = open('out.html', 'w')
 f.write('<html>')
 f.write('<p>')
 
-for i in output:
-    word = i[0]
-    count = 0
-    for line in line_list:
-        if word in line.lower() and count < 3:
-            if line not in sentences_dict:
-                for x in line.split():
-                    if x in list_keywords:
-                        write_yellow(f, x)
-                        f.write(' ')
-                    else:
-                        f.write(x + ' ')
-                sentences_dict[line] = 1
-                count += 1
-                f.write('.<br><br>')
+for line in line_list:
+    if '<span style' in line:
+        f.write(line)
+
+# for i in output:
+#     word = i[0]
+#     count = 0
+#     for line in line_list:
+#         if word in line.lower() and count < 3:
+#             if line not in sentences_dict:
+#                 for x in line.split():
+#                     if x in list_keywords:
+#                         write_yellow(f, x)
+#                         f.write(' ')
+#                     else:
+#                         f.write(x + ' ')
+#                 sentences_dict[line] = 1
+#                 count += 1
+#                 f.write('.<br><br>')
 f.write('</p>')
 f.write('</html>')
 f.close()
--- webpage.py
+++ webpage.py
@@ -3,30 +3,30 @@ import pandas as pd
 import numpy as np
 from keybert import KeyBERT
 
-st.title('Privacy Helper')
+# st.title('Privacy Helper')
 
-st.sidebar.header("Select a company to view their data policy.")
-def user_input_company():
-    selection = st.sidebar.selectbox('Company',('Amazon','Facebook', 'Google', 'Grubhub', 'Instagram', 'LinkedIn', 'Snapchat', 'Tiktok', 'Twitch', 'Uber', 'Yahoo'))
-    return selection
+# st.sidebar.header("Select a company to view their data policy.")
+# def user_input_company():
+#     selection = st.sidebar.selectbox('Company',('Amazon','Facebook', 'Google', 'Grubhub', 'Instagram', 'LinkedIn', 'Snapchat', 'Tiktok', 'Twitch', 'Uber', 'Yahoo'))
+#     return selection
 
-company = user_input_company()
+# company = user_input_company()
 
-st.subheader("Here is a sample of the Privacy Policy we are looking at.")
+# st.subheader("Here is a sample of the Privacy Policy we are looking at.")
 
-company_file_name = company + 'DataPolicy.txt'
-company_file_path = 'privacy_policy_files/' + company_file_name
+# company_file_name = company + 'DataPolicy.txt'
+# company_file_path = 'privacy_policy_files/' + company_file_name
 
-text = ""
+# text = ""
 
-with open(company_file_path, encoding="utf-8") as f:
-    for i in range(15):
-        text += next(f).strip()
+# with open(company_file_path, encoding="utf-8") as f:
+#     for i in range(15):
+#         text += next(f).strip()
 
-st.markdown(text)
+# st.markdown(text)
 
-st.subheader("Here is the highlights of the Privacy Policy.")
-st.markdown("sample highlights")
+# st.subheader("Here is the highlights of the Privacy Policy.")
+# st.markdown("sample highlights")
 
 
 doc = """
@@ -43,6 +43,13 @@ doc = """
       """
 kw_model = KeyBERT()
 
-keywords = kw_model.extract_keywords(doc, highlight=True)
-st.text(keywords)
-st.markdown(keywords)
+f = open('out.html', 'w')
+f.write('<html><p>')
+f.write(kw_model.extract_keywords(doc, highlight=True))
+
+f.write('</p></html>')
+f.close
+
+# keywords = kw_model.extract_keywords(doc, highlight=True)
+# st.text(keywords)
+# st.markdown(keywords)
